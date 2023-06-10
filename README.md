e1914d05b0f1ce2c6e9b9c65f2b0ad9c3c021a2c
Vania <vania.vnce@gmail.com>
2023-06-10 02:46:10 -0400

    estructura html lista

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # js2
+
+Variables 
+DESAFIO PARTE 1
+    imagen = img-prn
+    Button = borde
+
+
+Validacion 
+-No debe aceptar valores negativos en los sticker
+
+
+
+
+Notas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Inicio 09/06/2023
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("conectado")
+
+function borde() {
+    let bordeRojo = document.querySelector('.imagen')
+    bordeRojo.style.border = 'solid 2px red'
+    borderojo.innerHTML()
+    classList.toggle('bordeRojo')
+}
+
